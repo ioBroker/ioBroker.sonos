7eb69b66d1940d115231beba61d1227a1b025d0c
Ingo Fischer <github@fischer-ka.de>
2019-03-11 22:47:48 +0100

    fix unit testing

--- test/unit.js
+++ test/unit.js
@@ -6,10 +6,15 @@ const jscToolsMock = {
 	getInstalledInfo() { return {}; },
 };
 
+const jscLogMock = () => {};
+
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.unit(path.join(__dirname, '..'), {
 	additionalMockedModules: {
 		"{CONTROLLER_DIR}/lib/tools.js": jscToolsMock,
 		"{CONTROLLER_DIR}/lib/tools": jscToolsMock,
+                "{CONTROLLER_DIR}/lib/logger.js": jscLogMock,
+                "{CONTROLLER_DIR}/lib/logger": jscLogMock,
+
 	}
 });
