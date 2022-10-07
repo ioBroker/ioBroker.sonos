3388d2f14ddbe6b60d17164066f25f6b999675b1
Sebastian <github.com@sebastiankoehler.de>
2022-10-07 21:18:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1858,15 +1858,16 @@ function processSonosEvents(event, data) {
  * @param {string} player Player object ID
  */
 async function updateHtmlQueue(player) {
-    let queue = this.getStateAsync(`${player}.queue_html`);
+    const playerDp = `root.${player}`
+    let queue = adapter.getStateAsync(`${playerDp}.queue_html`);
     queue = queue.replace('class="sonosQueueRow currentTrack"', 'class="sonosQueueRow"');
-    const trackNumber =  this.getStateAsync(`${player}.current_track_number`);
+    const trackNumber =  adapter.getStateAsync(`${playerDp}.current_track_number`);
     const regexString =  `<tr\sclass="sonosQueueRow"\sonclick="vis\.setValue\('sonos\.[0-9]\.root\.[0-9]{1,3}_[0-9]{1,3}_[0-9]{1,3}_[0-9]{1,3}\.current_track_number', ${trackNumber}\)`
     const regex = new RegExp(regexString);
     let match = queue.match(regex);
     const newString = match.replace('class="sonosQueueRow"', 'class="sonosQueueRow currentTrack"');
     queue = queue.replace(match, newString);
-    this.setStateAsync(`${player}.queue_html`, {val: queue, ack: true});
+    adapter.setStateAsync(`${playerDp}.queue_html`, {val: queue, ack: true});
 
     //adapter.setState({device: 'root', channel: ip, state: 'queue_html'},   {val: queue, ack: true});
 
@@ -2041,21 +2042,10 @@ function main() {
                 socketServer && socketServer.sockets.emit('transport-state', data);
                 processSonosEvents('transport-state', data);
 
-                adapter.log.debug(JSON.stringify(data));
-
-                const player = discovery.getPlayerByUUID(data.uuid);
-                adapter.log.info(JSON.stringify(player));
-
-                const ip = player._address;
-                adapter.log.info(ip);
-
-
-
-
-
-
-
                 //modify queue
+                const player = discovery.getPlayerByUUID(data.uuid);
+                const playerip = player._address;
+                updateHtmlQueue(playerip);
             });
 
             discovery.on('group-volume', data => {
