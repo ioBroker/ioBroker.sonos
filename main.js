9e38e992ff07d20c7d5344e82502b1cd39e601f6
Sebastian <github.com@sebastiankoehler.de>
2022-10-30 13:53:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1371,7 +1371,6 @@ function takeSonosState(ip, sonosState) {
     updateHtmlQueue(playerip, sonosState.trackNo);
 
     if (lastCover !== sonosState.currentTrack.albumArtUri) {
-        //const defaultImg = __dirname + '/img/browse_missing_album_art.png';
         const defaultImg = __dirname + '/img/no-cover.png';
         const stateName  = adapter.namespace + '.root.' + ip + '.cover_png';
         let fileName;
